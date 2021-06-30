d3ef198a1e139985cd6d41fd0e0acf499c4e72a6
thurpe <babatope.ifesanya@gmail.com>
2021-06-30 19:13:00 +0100

    Created new, edit partials and shared folder

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -13,10 +13,37 @@ class RecipesController < ApplicationController
     end
 
     def create
+        @recipe = Recipe.new(recipe_params)
+        @recipe.chef = Chef.find(1)
 
+        if @recipe.save
+            flash[:success] = "Your recipe has been created successfully!"
+            redirect_to recipes_path
+        else
+            render :new
+        end
+    end
+
+    def edit
+        @recipe = Recipe.find(params[:id])
+    end
+
+    def update
+        @recipe = Recipe.find(params[:id])
+        if @recipe.update(recipe_params)
+            flash[:success] = "Your recipe has been updated successfully!"
+            redirect_to recipes_path(@recipe)
+        else
+            render :edit
+        end
     end
 
     def save
 
+    end
+
+    private
+    def recipe_params
+        params.require(:recipe).permit(:name, :summary, :description)
     end
 end
